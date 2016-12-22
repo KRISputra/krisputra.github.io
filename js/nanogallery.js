c933118a1e0b2a1e79d1f0bb4247d4ef15dc94e8
Hafidz Nasruddin <hafidz@gmail.com>
2016-12-22 14:41:10 +0800

    Site updated: 2016-12-22 14:41:10

--- js/nanogallery.js
+++ js/nanogallery.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
         userID: '38635440@N07',
         
         // uncomment this line to display one specific album:
-        albumList:'72157677816378106|72157677816436796|72157674065240533|72157673486497604|72157673487045514|72157673978651874|72157677913316095',
+        //albumList:'72157677816378106|72157677816436796|72157674065240533|72157673486497604|72157673487045514|72157673978651874|72157677913316095',
+        whiteList:'KRISPutra',
         //albumSorting: 'reversed',
         // uncomment this line to display full photostream (v5.0.0):
         // photoset:'none',
